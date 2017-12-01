44287341eaa40ab17e7877cbb89cf4dda41d1be0
Peter Sepstrup <peter@sepstrupnet.dk>
2017-12-01 19:25:30 +0100

    Initial Commit

---
+++ EasyTablesError/EasyTablesError.h
@@ -0,0 +1,19 @@
+//
+//  EasyTablesError.h
+//  EasyTablesError
+//
+//  Created by Peter Sepstrup on 1/12/17.
+//  Copyright © 2017 powerLABS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyTablesError.
+FOUNDATION_EXPORT double EasyTablesErrorVersionNumber;
+
+//! Project version string for EasyTablesError.
+FOUNDATION_EXPORT const unsigned char EasyTablesErrorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyTablesError/PublicHeader.h>
+
+
